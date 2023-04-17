bcee2ed59e8248318a95626f304e3d98602257ee
DimitriJG <120719757+DimitriJG@users.noreply.github.com>
2023-04-17 04:55:25 +0200

    Scenario d (#18)
    * some sprites added
    
    * Changed the playerController to keep direction (unfinished)
    
    * Fixed previous implementation of button priorisation
    
    ---------
    
    Co-authored-by: TheBeacon95 <30535313+TheBeacon95@users.noreply.github.com>

--- Assets/Scripts/Entities.Impl/Player/PlayerControls.cs
+++ Assets/Scripts/Entities.Impl/Player/PlayerControls.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerControls", menuName = "Player/Controls")]
@@ -8,20 +10,7 @@ public class PlayerControls : ScriptableObject {
     public Vector2Int PressedDirection {
         // Todo for dimi: Make it so that the next direction is only taken if the previous one is not pressed anymore.
         get {
-            Vector2Int direction = Vector2Int.zero;
-            if (Input.GetKey(m_inputUp)) {
-                direction = Vector2Int.up;
-            }
-            else if (Input.GetKey(m_inputDown)) {
-                direction = Vector2Int.down;
-            }
-            else if (Input.GetKey(m_inputLeft)) {
-                direction = Vector2Int.left;
-            }
-            else if (Input.GetKey(m_inputRight)) {
-                direction = Vector2Int.right;
-            }
-            return direction;
+            return GetDirection();
         }
     }
 
@@ -29,6 +18,52 @@ public class PlayerControls : ScriptableObject {
         get {
             return Input.GetKey(m_inputFire);
         }
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    /// <summary>
+    /// Returns the direction the player wants to move in.
+    /// Note: When multiple directions are pressed, the direction that was pressed second has priority.
+    /// </summary>
+    /// <returns>Direction the player wants to move in.</returns>
+    private Vector2Int GetDirection() {
+        Vector2Int direction = Vector2Int.zero;
+        List<Vector2Int> currentlyPressedDirections = new List<Vector2Int>();
+        if (Input.GetKey(m_inputUp)) {
+            currentlyPressedDirections.Add(Vector2Int.up);
+        }
+        if (Input.GetKey(m_inputRight)) {
+            currentlyPressedDirections.Add(Vector2Int.right);
+        }
+        if (Input.GetKey(m_inputDown)) {
+            currentlyPressedDirections.Add(Vector2Int.down);
+        }
+        if (Input.GetKey(m_inputLeft)) {
+            currentlyPressedDirections.Add(Vector2Int.left);
+        }
+
+        if (currentlyPressedDirections.Count < 1) {
+            // Do nothing.
+            m_lastDirection = Vector2Int.zero;
+        }
+        else if (currentlyPressedDirections.Count == 1) {
+            direction = currentlyPressedDirections[0];
+            m_lastDirection = direction;
+        }
+        else {
+            foreach (Vector2Int currentlyPressedDirection in currentlyPressedDirections) {
+                if (currentlyPressedDirection != m_lastDirection) {
+                    direction = currentlyPressedDirection;
+                    break;
+                }
+            }
+        }
+
+        
+        return direction;
     }
 
     #endregion
@@ -45,6 +80,9 @@ public class PlayerControls : ScriptableObject {
     private KeyCode m_inputRight;
     [SerializeField]
     private KeyCode m_inputFire;
+
+    private Vector2Int m_lastDirection;
+    private static ReadOnlyCollection<Vector2Int> s_directions = (new List<Vector2Int>() { Vector2Int.up, Vector2Int.right, Vector2Int.down, Vector2Int.left }).AsReadOnly();
 
     #endregion
 }
